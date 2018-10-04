dd89c78122aabd11326fd3cf2fa45d9a87dc7fd6
Alejandro Ramírez H <soyalejoramirez@gmail.com>
2018-10-04 09:14:17 -0500

    Se modifica la prueba para que pase.

--- src/test/java/co/alejo/bookstore/BookstoreApplicationTests.java
+++ src/test/java/co/alejo/bookstore/BookstoreApplicationTests.java
@@ -1,5 +1,6 @@
-package co.alejo.libreria;
+package co.alejo.bookstore;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,10 +8,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class LibreriaApplicationTests {
+public class BookstoreApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		Assert.assertTrue(true);
 	}
 
 }
