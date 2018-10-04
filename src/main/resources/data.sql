65fa6167c1f31f8cfab6a414ed200679c541fa5d
Alejandro Ramírez <soyalejoramirez@gmail.com>
2018-10-04 16:23:46 -0500

    Se modifica el set de datos iniciales.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 DELETE BOOKS;
 
 INSERT INTO BOOKS (name, author, publisher, genre)
-VALUES ('Hambriento', 'NACH', 'Planeta', 'Poetry');
+VALUES ('The Vampire Diaries', 'LJ Smith', 'Hachette', 'Fantasy');
 
 INSERT INTO BOOKS (name, author, publisher, genre)
 VALUES ('El olvido que seremos', 'Hector Abad Faciolince', 'Planeta', 'Biography');
