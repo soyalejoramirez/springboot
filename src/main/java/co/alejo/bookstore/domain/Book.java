0c51869c44a001218188efe71e01b324d59f94b8
Alejandro Ramírez <soyalejoramirez@gmail.com>
2018-10-18 12:06:22 -0500

    Se agrega GenerationType.IDENTITY como estrategia de generación de consecutivo.

--- src/main/java/co/alejo/bookstore/domain/Book.java
+++ src/main/java/co/alejo/bookstore/domain/Book.java
@@ -2,6 +2,7 @@ package co.alejo.bookstore.domain;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -9,7 +10,7 @@ import javax.persistence.Table;
 @Table(name = "BOOKS")
 public class Book {
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int isbn;
 	private String name;
 	private String author;
