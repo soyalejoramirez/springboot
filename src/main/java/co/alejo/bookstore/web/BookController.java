4ae240fa77636becd4c1701e67e1ac76825b2b12
Alejandro Ramírez <soyalejoramirez@gmail.com>
2018-10-18 12:06:36 -0500

    Se agrega permisos de CrossOrigins

--- src/main/java/co/alejo/bookstore/web/BookController.java
+++ src/main/java/co/alejo/bookstore/web/BookController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,7 @@ import co.alejo.bookstore.domain.service.BookService;
 
 @RestController
 @RequestMapping("/api/books")
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class BookController {
 	@Autowired
 	private BookService bookService;
